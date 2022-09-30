6c11fad69b7a07491700b48aba888e617c015428
widici <84205124+widici@users.noreply.github.com>
2022-09-30 15:54:17 +0200

    Added comments to main

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from config import *
 text = input(": ")
 morse = ""
 
+# Encoder, for key in dict -> replaces char with dict value -> if char isn't equal to space -> prints char
 for char in text:
     for key in dict.keys():
         char = char.upper().replace(key, dict[key])
     sleep(letter_wait)
     if char != ' ':
         print(f"\n{rev_dict[char]}")
+    # Interpreter, for signal (+ -) in char -> prints char -> if signal equals - or + -> turns on led and sleeps | sleeps -> turns of led
     for signal in char:
         print(signal)
         if signal == "-":
@@ -23,3 +25,4 @@ for char in text:
             sleep(space)
         led.value(0)
         sleep(symbol_wait)
+        
